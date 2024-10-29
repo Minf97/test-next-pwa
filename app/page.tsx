9d66bf80bdfd4b2e26ae636e37c074a80397a21b
胥昱全 <287728237@qq.com>
2024-10-29 12:22:50 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function Page() {
+  
+  return (
+    <>
+      <h1>Next.js + Serwist</h1>
+    </>
+  );
+}
